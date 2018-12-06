8582c14054edf09a746a2232af0b4ae7485d7167
YusofBandar <ybandar1902@gmail.com>
2018-12-06 16:04:09 +0000

    throttle slider added

--- src/app/shared/carControl/carControlCtrl.js
+++ src/app/shared/carControl/carControlCtrl.js
@@ -2,11 +2,19 @@ angular.module('app').controller('CarControlViewCtrl', CarControlViewCtrl);
 
 CarControlViewCtrl.$inject = [
     '$rootScope',
+    '$scope',
     'dataService'
 ];
 
-function CarControlViewCtrl($rootScope,dataService) {
+function CarControlViewCtrl($rootScope,$scope,dataService) {
     var vm = this;
 
+    vm.throttle = 0;
+
+    $scope.$watch("carControlView.throttle",function(newThrottle,oldThrottle){
+        if(newThrottle != oldThrottle){
+            console.log(newThrottle);
+        }
+    })
    
 }
