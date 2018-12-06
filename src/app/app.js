1c59f71bea15c2d3c2022e86f8cbde5807058637
YusofBandar <ybandar1902@gmail.com>
2018-12-06 13:56:18 +0000

    angularjs setup routing to home page

---
+++ src/app/app.js
@@ -0,0 +1,35 @@
+angular.module('app', ['ui.router']);
+
+
+angular.module('app').config(config);
+
+config.$inject = [
+    '$stateProvider',
+    '$urlRouterProvider'
+]
+
+function config($stateProvider, $urlRouterProvider) {
+    var indexState = {
+        name: 'index',
+        url: '/index',
+        templateUrl: 'app/shared/index/indexView.html',
+        controller: 'IndexViewCtrl',
+        controllerAs: 'indexView'
+    }
+
+    $stateProvider.state(indexState);
+
+    $urlRouterProvider.otherwise('/index');
+}
+
+angular.module('app').run(run);
+
+run.$inject = [
+    '$rootScope'
+]
+
+function run($rootScope) {
+    console.log('version 1.0.0');
+
+    $rootScope.url = '192.168.1.102';
+}
---
+++ src/app/shared/index/IndexViewCtrl.js
@@ -0,0 +1,7 @@
+angular.module('app').controller('IndexViewCtrl', IndexViewCtrl);
+
+IndexViewCtrl.$inject = [];
+
+function IndexViewCtrl(){
+    console.log('hi from controller');
+}
