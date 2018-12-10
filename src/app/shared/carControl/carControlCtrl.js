e1bd0753f64b30cdcaa3db5b9e1dc0c0052e032c
Yusof Bandar <ybandar1902@gmail.com>
2018-12-10 14:29:29 +0000

    Formatted CarControlViewCtrl for better reading

--- src/app/shared/carControl/carControlCtrl.js
+++ src/app/shared/carControl/carControlCtrl.js
@@ -8,9 +8,9 @@ CarControlViewCtrl.$inject = [
     'dataService'
 ];
 
-function CarControlViewCtrl($scope,$state,$stateParams,$window,dataService) {
+function CarControlViewCtrl($scope, $state, $stateParams, $window, dataService) {
     var vm = this;
-	var changed = false;
+    var changed = false;
     const DEFAULT_THROTTLE = 0;
     var channel;
     var ip_address;
@@ -23,52 +23,51 @@ function CarControlViewCtrl($scope,$state,$stateParams,$window,dataService) {
 
     activate();
 
-    function activate(){
+    function activate() {
         console.log($stateParams);
-        if($stateParams.channel === null || $stateParams.ip_address.length === null){
-            $state.transitionTo('index',{});
+        if ($stateParams.channel === null || $stateParams.ip_address.length === null) {
+            $state.transitionTo('index', {});
         }
         channel = $stateParams.channel;
         address = $stateParams.ip_address;
     }
 
-    function stop(){
-        dataService.stop(ip_address).then(function(result){
-		$state.transitionTo('index',{});
-	}).catch(function(error){
-		alert("Server Error");
-		$state.transitionTo('index',{});
-	})
+    function stop() {
+        dataService.stop(ip_address).then(function (result) {
+            $state.transitionTo('index', {});
+        }).catch(function (error) {
+            alert("Server Error");
+            $state.transitionTo('index', {});
+        })
     }
 
- 	window.onhashchange = function(){
-		if(changed){
-		console.log('changed');
-		stop();
-		}else{
-		changed=true;
-		}
-		
-	}
+    window.onhashchange = function () {
+        if (changed) {
+            console.log('changed');
+            stop();
+        } else {
+            changed = true;
+        }
+    }
 
-    $scope.$watch("carControlView.throttle",function(newThrottle,oldThrottle){
-        if(newThrottle != oldThrottle){
-            dataService.setThrottle(ip_address,channel,newThrottle).then(function(result){
-                
-		result = result.data;
-		for(var i=0;i<result.length;i++){
-			
-			if(result[i].channel === channel){
-				console.log('found channel');
-				vm.actualThrottle = result[i].percent;
-				break;
-			} 
-		}
-            }).catch(function(error){
+    $scope.$watch("carControlView.throttle", function (newThrottle, oldThrottle) {
+        if (newThrottle != oldThrottle) {
+            dataService.setThrottle(ip_address, channel, newThrottle).then(function (result) {
+
+                result = result.data;
+                for (var i = 0; i < result.length; i++) {
+
+                    if (result[i].channel === channel) {
+                        console.log('found channel');
+                        vm.actualThrottle = result[i].percent;
+                        break;
+                    }
+                }
+            }).catch(function (error) {
                 console.log(error);
                 vm.throttleError = true;
             })
         }
     })
-   
+
 }
