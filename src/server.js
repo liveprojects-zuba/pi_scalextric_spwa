89b3cc9aeae354edf946df7c6641c287d15821e9
unknown <ybandar1902@gmail.com>
2019-01-05 23:45:28 +0000

    Pi-Details Are Provided by Env Vars

--- src/server.js
+++ src/server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const http = require('http')
 const path = require('path');
+var fs = require('fs');
+
+
+var getClientConfig = function () {
+  var result = {};
+
+  if(!process.env.HOST) throw new Error("HOST is not defined");
+  if(!process.env.CHANNELS) throw new Error("CHANNELS is not defined");
+  
+  result.HOST = process.env.HOST;
+  result.CHANNELS = process.env.CHANNELS;
+  
+  return result;
+}
+
+var writeClientConfig = function(config){
+  var client_config = config;
+  client_config = "angular.module('app').constant('piDetails'," + JSON.stringify(client_config) + ");";
+  fs.writeFileSync('./app/components/piDetails/piDetailsConstant.js',client_config);
+}
+
+writeClientConfig(getClientConfig());
+
 
 const app = express();
-
 
 app.use(express.static(path.join(__dirname, '')));
 
