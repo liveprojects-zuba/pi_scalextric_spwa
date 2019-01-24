f0d4a05030f0e25e7433ba92c7271231479cae55
YusofBandar <ybandar1902@gmail.com>
2019-01-24 18:32:01 +0000

    Added Mqtt Service

---
+++ src/app/components/mqtt/mqttService.js
@@ -0,0 +1,86 @@
+angular.module('app').service('mqttService', mqttService);
+
+mqttService.$inject = [
+];
+
+
+/*
+    Mqtt Service uses Eclipse Paho JavaScript Client found :
+        - https://github.com/eclipse/paho.mqtt.javascript
+        - https://web.archive.org/web/20181212171208/https://github.com/eclipse/paho.mqtt.javascript
+*/
+function mqttService() {
+    var self = this;
+
+    self.initialize = initialize;
+    self.connect = connect;
+    self.subscribe = subscribe;
+    self.publish = publish;
+    self.onConnectionLost = onConnectionLost;
+    self.onMessageArrived = onMessageArrived;
+
+    var client = null;
+
+    // Initialize mqtt client, this must be the done before any other actions
+    function initialize(hostname, port, clientId = "clientId") {
+        if (!hostname) { throw new Error("Invalid hostname") }
+        if (!port) { throw new Error("Invalid port")}
+        
+        client = new Paho.MQTT.Client(hostname, Number(port), clientId);
+    }
+
+    //connect to the mqtt broker
+    function connect(callback,username,password,ssl=false) {
+        if (!client) { throw new Error("Need to Initialize Mqtt") }
+        if (callback && typeof callback !== 'function') { throw new Error("Callback must be a function")}
+
+        var mqttOptions = {
+            onSuccess : callback,
+            onFailure : function(error){
+                console.error(error);
+            },
+            useSSL : ssl
+        }
+
+        if(username) mqttOptions.userName = username;
+        if(password) mqttOptions.password = password;
+        
+        client.connect(mqttOptions);
+    }
+
+    //subscribe to a mqtt topic, when message arrives client.onMessageArrived is called
+    function subscribe(topic) {
+        if (!client) { throw new Error("Need to Initialize Mqtt")}
+        if (!topic) { throw new Error("Need to define a topic")}
+
+        client.subscribe(topic)
+    }
+
+    //publish mqtt message
+    function publish(topic,message){
+        if (!client) { throw new Error("Need to Initialize Mqtt")}
+        if (!topic) {throw new Error("Need to define a topic")}
+        if (!message) {throw new Error("Need to define a message")}
+
+        var mqtt_message = new Paho.MQTT.Message(message);
+        mqtt_message.destinationName = topic;
+        client.send(mqtt_message);
+    }
+
+    //called when connection is lost
+    function onConnectionLost(callback) {
+        if (!client) { throw new Error("Need to Initialize Mqtt") }
+        if (callback && typeof callback !== 'function') { throw new Error("Callback must be a function")}
+        
+        client.onConnectionLost = callback;
+    }
+
+    // called when a message arrives
+    function onMessageArrived(callback) {
+        if (!client) { throw new Error("Need to Initialize Mqtt") }
+        if (callback && typeof callback !== 'function') { throw new Error("Callback must be a function")}
+
+        client.onMessageArrived = callback;
+    }
+
+}
